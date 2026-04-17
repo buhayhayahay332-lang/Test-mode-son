beb166a5e5a56353be53cd35c07b591241ae40d4
Nil-Son <buhayhayahay332@gmail.com>
2026-04-16 20:11:44 -0700

    upd

--- OperationOne-main/EspLib.lua
+++ OperationOne-main/EspLib.lua
@@ -382,7 +382,7 @@ local function ProcessESP(model, espData)
     local w = math.max(2, x1 - x0)
     local h = math.max(2, y1 - y0)
 
-    local padX = math.max(2, w * 1)
+    local padX = math.max(2, w * 0.10)
     local padY = math.max(2, h * 0.07)
     x0 = x0 - padX
     x1 = x1 + padX
