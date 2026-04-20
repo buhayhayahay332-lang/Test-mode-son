71083d65ae02636200d4a6cc8a9de3e4789501ae
Nil-Son <buhayhayahay332@gmail.com>
2026-04-19 22:05:49 -0700

    upd

--- OperationOne-main/yenofurry.lua
+++ OperationOne-main/yenofurry.lua
@@ -1,3 +1,4 @@
+  pcall(function() setthreadidentity(8) end)
     local cloneref    = cloneref or function(o) return o end
     local clonefunc   = clonefunction or function(f) return f end
     local newcclosure = newcclosure or function(f) return f end
