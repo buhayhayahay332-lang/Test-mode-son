35593291871598bf3904629051fb9701cbff0408
Nil-Son <buhayhayahay332@gmail.com>
2026-07-26 00:43:04 -0700

    test again

--- OperationOne-main/main.lua
+++ OperationOne-main/main.lua
@@ -805,7 +805,7 @@ local function applyDefaults()
 end
 
 local function runStartupInit()
-    local initOrder = { "silent_aim", "auto_shoot", "homing_projectiles", "gun_modification", "misc", ESP_MODULE_NAME, "fullbright" }
+    local initOrder = { "silent_aim", "auto_shoot", "homing_projectiles", "misc", "gun_modification", ESP_MODULE_NAME, "fullbright" }
     for _, name in ipairs(initOrder) do initModule(name, false) end
     applyDefaults()
     log("init complete")
